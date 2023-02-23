e81177dd7c7da70dfdf56e77e33174ac7508e1fa
Devram <kandharedeva87@gmail.com>
2023-02-23 18:38:04 +0530

    added state management and calculator functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,46 +1,75 @@
+import React, { useState } from "react";
 import "./App.scss";
 
 function Display(props) {
-  return <div className="calculator-display">{props.result}</div>;
+  return (
+    <div className="calculator-display">
+      <div className="result">{props.result}</div>
+    </div>
+  );
 }
 
-function Cell({ lastCell, content }) {
+function Cell({ lastCell, content, addToDisplayString }) {
   if (lastCell) {
-    return <div className="orange-cell">{content}</div>;
+    return (
+      <div className="orange-cell" onClick={() => addToDisplayString(content)}>
+        {content}
+      </div>
+    );
   }
-  return <div className="calculator-cell">{content}</div>;
+  return (
+    <div
+      className="calculator-cell"
+      onClick={() => addToDisplayString(content)}
+    >
+      {content}
+    </div>
+  );
 }
 
 function App() {
+  const [result, setResult] = useState("");
+
+  function addToDisplayString(content) {
+    if (content === "=") calculateResult();
+    else setResult(result + content);
+  }
+
+  function calculateResult() {
+    setResult(eval(result));
+  }
+
   return (
     <div className="app-container">
       <div className="calculator-app">
-        <Display result={"131.5"} />
+        <Display result={result} />
         <div className="calculator-row">
-          <Cell content="7" />
-          <Cell content="8" />
-          <Cell content="9" />
-          <Cell content="/" lastCell />
+          <Cell content="7" addToDisplayString={addToDisplayString} />
+          <Cell content="8" addToDisplayString={addToDisplayString} />
+          <Cell content="9" addToDisplayString={addToDisplayString} />
+          <Cell content="/" addToDisplayString={addToDisplayString} lastCell />
         </div>
         <div className="calculator-row">
-          <Cell content="4" />
-          <Cell content="5" />
-          <Cell content="6" />
-          <Cell content="*" lastCell />
+          <Cell content="4" addToDisplayString={addToDisplayString} />
+          <Cell content="5" addToDisplayString={addToDisplayString} />
+          <Cell content="6" addToDisplayString={addToDisplayString} />
+          <Cell content="*" addToDisplayString={addToDisplayString} lastCell />
         </div>
         <div className="calculator-row">
-          <Cell content="1" />
-          <Cell content="2" />
-          <Cell content="3" />
-          <Cell content="+" lastCell />
+          <Cell content="1" addToDisplayString={addToDisplayString} />
+          <Cell content="2" addToDisplayString={addToDisplayString} />
+          <Cell content="3" addToDisplayString={addToDisplayString} />
+          <Cell content="+" addToDisplayString={addToDisplayString} lastCell />
         </div>
         <div className="calculator-row">
-          <Cell content="." />
-          <Cell content="0" />
-          <Cell content="=" />
-          <Cell content="-" lastCell />
+          <Cell content="." addToDisplayString={addToDisplayString} />
+          <Cell content="0" addToDisplayString={addToDisplayString} />
+          <Cell content="=" addToDisplayString={addToDisplayString} />
+          <Cell content="-" addToDisplayString={addToDisplayString} lastCell />
         </div>
-        <div className="calculator-clear">Clear</div>
+        <div className="calculator-clear" onClick={() => setResult("")}>
+          Clear
+        </div>
       </div>
     </div>
   );
