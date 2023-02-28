a0ec00d6a2b2754559323bf201c0b51a7051d61d
Devram <kandharedeva87@gmail.com>
2023-02-28 22:44:32 +0530

    completed app with search filter which search the states in india

---
+++ search-filter/src/App.js
@@ -0,0 +1,37 @@
+import "./App.scss";
+import { useState } from "react";
+import searchIcon from "./search-icon.png";
+import { states } from "./state";
+
+function App() {
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <div className="app-container">
+      <div className="search-container">
+        <div className="search">
+          <input
+            className="search-text"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <img src={searchIcon} alt="search-icon" />
+        </div>
+        <div className="search-result-container">
+          {states
+            .filter((theState) =>
+              theState.name.toUpperCase().startsWith(searchText.toUpperCase())
+            )
+            .map((theState) => (
+              <div key={theState.id} className="search-result">
+                {theState.name}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
