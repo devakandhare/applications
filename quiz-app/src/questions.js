0d4e4c4ab20e8788806604b3761f71de3cf91f66
Devram <kandharedeva87@gmail.com>
2023-03-03 09:44:24 +0530

    completed quiz app with four basic questions and dsplay for final score

---
+++ quiz-app/src/questions.js
@@ -0,0 +1,42 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    options: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Paris", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Who is the CEO of the tesla?",
+    options: [
+      { answerText: "Jeff Bazos", isCorrect: false },
+      { answerText: "Elon Musk", isCorrect: true },
+      { answerText: "Bill Gates", isCorrect: false },
+      { answerText: "Tony Shark", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "The iPhone created by which company?",
+    options: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "How many Harry Potter books are there?",
+    options: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "4", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+    ],
+  },
+];
